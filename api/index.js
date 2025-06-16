da613cdd782f5d98e25f87d4d397133b7a6c6dc4
Marco-Alvarez <137237087+Marcos-Alvarez2004@users.noreply.github.com>
2025-06-16 13:46:58 -0300

    change route cors

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://ecommerce-hhyl.vercel.app/",
+    origin: "https://ecommerce-hhyl.vercel.app",
     credentials: true,
   })
 );
